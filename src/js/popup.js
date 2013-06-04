9b2a0facca43c0fbd5f56c9f1c5c40644c2336c6
Charles Gravolet <cgravolet@gmail.com>
2013-06-04 11:08:11 -0400

    Began implementing new UI design for popup

---
+++ src/js/popup.js
@@ -0,0 +1,72 @@
+var scroblrView = (function (model, Mustache) {
+	var $body = $("body");
+
+	function attachBehaviors () {
+		$body.on("click", "#authorizeBtn", function (e) {
+			e.preventDefault();
+			model.messageHandler({
+				name: "authButtonClicked"
+			});
+		});
+
+		$body.on("click", ".goto-section", function (e) {
+			e.preventDefault();
+			displaySection($(this).attr("href").slice(1));
+		});
+
+		$body.on("click", ".goto-link", function (e) {
+			e.preventDefault();
+			openNewTab($(this).attr("href"));
+		});
+
+		chrome.extension.onMessage.addListener(messageHandler);
+	}
+
+	function displaySection(section) {
+		$body.removeClass().addClass("show-" + section);
+	}
+
+	function initialize() {
+		attachBehaviors();
+
+		if (!model.lf_session) {
+			$body.addClass("show-authenticate");
+		} else {
+			renderNowPlaying();
+			$body.addClass("show-now-playing");
+		}
+	}
+
+	function messageHandler (msg) {
+		switch (msg.name) {
+		}
+		console.log(msg.name, msg.message);
+	}
+
+	function openNewTab(url) {
+		var newTab;
+
+		if (typeof chrome != "undefined") {
+			chrome.tabs.create({url: url});
+		} else if (typeof safari != "undefined") {
+			newTab = safari.application.activeBrowserWindow.openTab();
+			newTab.url = url;
+		}
+	}
+
+	function renderNowPlaying() {
+		var $nowPlaying, data, template, track;
+
+		$nowPlaying = $("section.now-playing");
+		template    = $.trim($("#tmplNowPlaying").html());
+		track       = model.currentTrack;
+		data        = {
+			host:    track ? track.host : null,
+			playing: track ? true : false
+		};
+
+		$nowPlaying.html(Mustache.render(template, data));
+	}
+
+	initialize();
+}(chrome.extension.getBackgroundPage(), Mustache));
