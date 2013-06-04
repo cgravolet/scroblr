571d8b73f0d7a4d8b2945f6f4aff1dfc489e4c58
Charles Gravolet <cgravolet@gmail.com>
2013-06-04 19:49:24 -0400

    Fixed jango plugin

--- src/js/plugins/jango.js
+++ src/js/plugins/jango.js
@@ -4,10 +4,11 @@
 
 	plugin.scrape = function () {
 		return {
-			artist:   $("#player_info #player_current_artist").contents().last().text(),
-			duration: calculateDuration($("#player_info #timer").text().substring(1)),
-			stopped:  $("#btn-playpause").hasClass("pause"),
-			title:    $("#player_info #current-song").text().replace(/^\s+/, "").replace(/\s+$/, "")
+			artist:   $("#player_info #player_current_artist > *").not("span").text(),
+			album:    $("#player_info #player_current_artist").contents().last().text(),
+			duration: scroblr.utilities.calculateDuration($("#player_info #timer").text().substring(1)),
+			stopped:  !$("#btn-playpause").hasClass("pause"),
+			title:    $("#player_info #current-song").text()
 		};
 	};
 }(Zepto));
