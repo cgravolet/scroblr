b23d1cc4373ae67ed2a54ffa3555008b61e8f343
Charles Gravolet <cgravolet@gmail.com>
2013-06-04 22:30:26 -0400

    Fixed amazon plugin

--- src/js/plugins/amazon.js
+++ src/js/plugins/amazon.js
@@ -5,7 +5,7 @@
 	plugin.scrape = function () {
 		return {
 			artist:   $("#nowPlayingSection .currentSongDetails .title").next().text().substring(3),
-			duration: calculateDuration($("#nowPlayingSection .currentSongStatus #currentTime").next().next().text()),
+			duration: scroblr.utilities.calculateDuration($("#nowPlayingSection .currentSongStatus #currentTime").next().next().text()),
 			stopped:  $("#mp3Player .mp3Player-MasterControl .mp3MasterPlayGroup").hasClass("paused"),
 			title:    $("#nowPlayingSection .currentSongDetails .title").text()
 		};
