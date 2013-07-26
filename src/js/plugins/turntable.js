f142b0ecb407c3294e3cfda2d3dad453d151b510
unmanbearpig <unmanbearpig@gmail.com>
2013-07-27 02:15:54 +0400

    Fixed another scrape issue
    Turntable doesn't load song log at page load any more
    so I think we can take all requred info from the main page.
    I don't know how to get song score though.

--- src/js/plugins/turntable.js
+++ src/js/plugins/turntable.js
@@ -5,12 +5,13 @@
 	plugin.hostre = new RegExp("turntable\\.fm", "i");
 	plugin.scrape = function () {
 		var info = {};
-		if ($('.songboard-title.songboard-main').length && $('.songboard-title.songboard-main').text().length) {
-			info.artist = $('#song-log-panel .song:first-child .details > span').contents();
-			info.duration = scroblr.utilities.calculateDuration(info.artist[info.artist.length - 1].textContent);
-			info.artist = info.artist[0].textContent;
-			info.title = $('#song-log-panel .song:first-child .title').text();
-			info.score = parseFloat($('#song-log-panel .song:first-child .score').text().replace(/[^0-9]+/g, ''));
+		if ($('.songboard-artist.songboard-main').length && $('.songboard-artist.songboard-main').text().length) {
+			info.artist = $('.songboard-artist.songboard-main')[0].textContent;
+			info.title = $('.songboard-song.songboard-main')[0].textContent;
+
+			time = $('.songboard-time').contents()[0].textContent;
+			time_left = $('.songboard-time-left').contents()[0].textContent;
+			info.duration = scroblr.utilities.calculateDuration(time) + scroblr.utilities.calculateDuration(time_left)
 		}
 		return info;
 	};
