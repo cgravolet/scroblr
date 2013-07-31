d0335365194cf2f8fd5a40f4716a8fed8dc67f4d
Charles Gravolet <cgravolet@gmail.com>
2013-07-31 17:56:10 -0400

    Fixed validation errors in turntable plugin

--- src/js/plugins/turntable.js
+++ src/js/plugins/turntable.js
@@ -4,14 +4,16 @@
 
 	plugin.hostre = new RegExp("turntable\\.fm", "i");
 	plugin.scrape = function () {
-		var info = {};
-		if ($('.songboard-artist.songboard-main').length && $('.songboard-artist.songboard-main').text().length) {
-			info.artist = $('.songboard-artist.songboard-main')[0].textContent;
-			info.title = $('.songboard-song.songboard-main')[0].textContent;
+		var info, time, timeleft;
 
-			time = $('.songboard-time').contents()[0].textContent;
-			time_left = $('.songboard-time-left').contents()[0].textContent;
-			info.duration = scroblr.utilities.calculateDuration(time) + scroblr.utilities.calculateDuration(time_left)
+		info = {};
+
+		if ($('.songboard-artist.songboard-main').length && $('.songboard-artist.songboard-main').text().length) {
+			info.artist   = $('.songboard-artist.songboard-main')[0].textContent;
+			info.title    = $('.songboard-song.songboard-main')[0].textContent;
+			time          = $('.songboard-time').contents()[0].textContent;
+			timeleft      = $('.songboard-time-left').contents()[0].textContent;
+			info.duration = scroblr.utilities.calculateDuration(time, timeleft);
 		}
 		return info;
 	};
