fb3e0839877319918da5803f5137f34d52e94569
Charles Gravolet <cgravolet@gmail.com>
2013-08-03 20:14:36 -0400

    Fixed accuradio scrobbling

--- src/js/plugins/accuradio.js
+++ src/js/plugins/accuradio.js
@@ -3,17 +3,17 @@
 	var plugin = scroblr.registerHost("accuradio");
 
 	plugin.scrape = function () {
-		var artist = $("#span_information_artist").text() || "";
+		var artist = $("#songartist").text() || "";
 
 		if (artist.indexOf("Click here") >= 0) {
 			artist = "";
 		}
 
 		return {
-			album:   $("#span_information_album").text(),
+			album:   $("#songalbum").text(),
 			artist:  artist,
-			title:   $("#span_information_title").text(),
-			stopped: $("#player_lowest_controls_wrapper #play").length ? true : false
+			title:   $("#songtitle").text(),
+			stopped: $("#playerPlayButton").length ? true : false
 		};
 	};
 }(Zepto));
