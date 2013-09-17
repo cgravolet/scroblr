bd677b3a1f1bfe29ca99c839cf60b0bbbef721aa
Charles Gravolet <cgravolet@gmail.com>
2013-09-16 20:03:10 -0400

    Fixed soundcloud scrobbling issue (duration/elapsed times no longer available)

--- src/js/plugins/soundcloud.js
+++ src/js/plugins/soundcloud.js
@@ -16,8 +16,6 @@
 			if (!info.stopped) {
 				player        = playing.parents(".sound");
 				info.artist   = player.find(".soundTitle__username").text();
-				info.duration = scroblr.utilities.calculateDuration(player.find(".timeIndicator__total").text().replace(".", ":"));
-				info.elapsed  = scroblr.utilities.calculateDuration(player.find(".timeIndicator__current").text().replace(".", ":"));
 				info.title    = player.find(".soundTitle__title").text();
 			}
 		} else {
@@ -29,8 +27,6 @@
 			if (!info.stopped) {
 				player        = playing.parents("div.player");
 				info.artist   = player.find(".user-name").text();
-				info.duration = scroblr.utilities.calculateDuration(player.find(".timecodes span:last").text().replace(".", ":"));
-				info.elapsed  = scroblr.utilities.calculateDuration(player.find(".timecodes span:first").text().replace(".", ":"));
 				info.title    = player.find("h3 a").text();
 			}
 		}
