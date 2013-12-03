6de9458659cbf1d2463ff055c827a761de138815
Charles Gravolet <cgravolet@gmail.com>
2013-12-03 11:46:27 -0500

    Fixed songza scrobbling issue

--- src/js/plugins/songza.js
+++ src/js/plugins/songza.js
@@ -2,12 +2,22 @@
 
 	var plugin = scroblr.registerHost("songza");
 
+	plugin.test = function () {
+		var domainTest, playerTest;
+
+		domainTest = this.hostre.test(document.location.hostname);
+		playerTest = $("#player").length > 0;
+
+		return domainTest && playerTest;
+	};
+
 	plugin.scrape = function () {
-		return {
-			artist:  $("#player .szi-roll-song .szi-info .szi-artist").text(),
+		var info = {
+			artist:  $("#player .szi-artist").text(),
 			percent: parseFloat($("#player .szi-progress .szi-bar").width() / $("#player .szi-progress").width()),
-			stopped: !!$("#player .sz-player-state-pause").length,
-			title:   $("#player .szi-roll-song .szi-info .szi-title").text()
+			stopped: $("#player .player-play").css("display") === "none" ? false : true,
+			title:   $("#player .szi-title").text()
 		};
+		return info;
 	};
 }(Zepto));
