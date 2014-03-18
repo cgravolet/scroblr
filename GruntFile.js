e903f826461c006c6b2a296af26a296d20ca6d4a
Charles Gravolet <cgravolet@gmail.com>
2014-03-18 19:58:57 -0400

    Loaded watch grunt task

--- GruntFile.js
+++ GruntFile.js
@@ -13,6 +13,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
