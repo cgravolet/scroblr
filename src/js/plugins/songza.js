39a15462961c602406d1faf8a4acc8ff545309d9
Charles Gravolet <cgravolet@gmail.com>
2014-03-22 15:16:54 -0400

    Added support for new.songza.com

--- src/js/plugins/songza.js
+++ src/js/plugins/songza.js
@@ -10,16 +10,35 @@ songza.test = function () {
     var domainTest = this.hostre.test(document.location.hostname);
     var playerTest = $("#player").length > 0;
 
+	if (domainTest) {
+		this.isNewSongza = /new\.songza\.com/i.test(document.location.hostname);
+	}
+
     return domainTest && playerTest;
 };
 
 songza.scrape = function () {
-    var info = {
-        artist:  $("#player .szi-artist").text(),
-        percent: parseFloat($("#player .szi-progress .szi-bar").width() / $("#player .szi-progress").width()),
-        stopped: $("#player .player-play").css("display") === "none" ? false : true,
-        title:   $("#player .szi-title").text()
-    };
+	var $player = $("#player");
+	var info    = {};
+
+	if (this.isNewSongza) {
+		info.artist = $(".miniplayer-info-artist-name a",
+				$player).text().replace(/^by\s+/i, "");
+		info.title = $(".miniplayer-info-track-title", $player).text();
+		info.stopped = ($(".miniplayer-control-play-pause .ui-icon-ios7-play",
+				$player).css("display") === "none" ? false : true);
+		info.album = $(".miniplayer-info-album-title", $player).text().replace(
+				/^from\s+/i, "");
+		info.percent = $(".miniplayer-timeline-current-time", $player).width() /
+				$(".miniplayer-timeline", $player).width();
+	} else {
+		info.artist  = $(".szi-artist", $player).text();
+		info.title   = $(".szi-title", $player).text();
+		info.stopped = ($(".player-play", $player).css("display") === 
+				"none" ? false : true);
+		info.percent = parseFloat($(".szi-progress .szi-bar", $player).width() /
+				$(".szi-progress", $player).width());
+	}
 
     return info;
 };
