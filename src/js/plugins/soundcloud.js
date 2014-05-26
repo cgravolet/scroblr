8ffcd3c71ca742f8484d57b647c7588d8e8529a4
Sonak Patel <sonakspatel@gmail.com>
2014-05-26 19:06:44 +0100

    #40 bug fixes on Soundcloud; sounds and playlists are now found both in stream and sound mode
    Original play button selector excludes play log, it was picking up the
    play button in the recently played log (which uses the medium btn
    class).
    With no API to readily extract the current song, each case (playlist in
    stream mode, individual sounds and playlists) are now picked up.

--- src/js/plugins/soundcloud.js
+++ src/js/plugins/soundcloud.js
@@ -7,21 +7,27 @@ var soundcloud = Object.create(Plugin);
 soundcloud.init("soundcloud", "SoundCloud");
 
 soundcloud.scrape = function () {
-    var info, player, playing, soundcloudNext, listingInfo;
+    var info, player, playing, soundcloudNext, compactArr;
 
     soundcloudNext = !!$("body > #app").length;
 
     if (soundcloudNext) {
-        playing = $(".sc-button-play.sc-button-pause");
+        playing = $(".sc-button-play.sc-button-pause").not(".sc-button-medium").last();
         info = {
             stopped: !playing.length
         };
 
-        listingInfo = playing.parents(".trackList__item").length > 0 ? ".trackList__item" : ".sound";
         if (!info.stopped) {
-            player        = playing.parents(listingInfo);
-            info.artist   = player.find(".soundTitle__username").text();
-            info.title    = player.find(".soundTitle__title").text();
+            if(playing.parents(".streamContext").length && playing.parents(".playlist").length){
+                player        = playing.parents(".playlist").find(".active");
+                compactArr    = player.find(".compactTrackListItem__content").text().split(/-(.+)?/);
+                info.artist   = compactArr[0];
+                info.title    = compactArr[1];
+            } else {
+                player        = playing.parents(playing.parents(".sound").length ? ".sound" : ".trackList__item");
+                info.artist   = player.find(".soundTitle__username").text();
+                info.title    = player.find(".soundTitle__title").text();
+            }
         }
     } else {
         playing = $(".play.playing");
