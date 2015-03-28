190a71eed1e158ad61b44f8f7c51b764f52264fe
Stella Tigre <stella.cannefax@gmail.com>
2015-03-28 00:52:37 -0500

    Add album support, fix Discover detection

--- src/js/plugins/bandcamp.js
+++ src/js/plugins/bandcamp.js
@@ -8,9 +8,12 @@ var bandcamp = Object.create(Plugin);
 bandcamp.init("bandcamp", "Bandcamp");
 
 bandcamp.scrape = function () {
-    var discover, info, isTrack, pageTitle;
+    var discover, info, isTrack, isAlbum, pageTitle;
 
-    discover = window.location.pathname.slice(1) === "discover";
+    discover = window.location.hash === "#discover";
+    isTrack = window.location.pathname.split("/")[1] === "track";
+    isAlbum = window.location.pathname.split("/")[1] === "album";
+
     info = {
         stopped: !$(".inline_player .playbutton").hasClass("playing")
     };
@@ -22,6 +25,16 @@ bandcamp.scrape = function () {
         } else {
             info.artist = $("span[itemprop=byArtist]").text();
         }
+
+        if (isTrack) {
+            info.album = $("#name-section h3 span a span").text();
+        } else if (isAlbum) {
+            info.album = $("#name-section > h2").text().trim();
+        } else if (discover) {
+            info.album = $("div.itemtext a").filter(function (i, e) { 
+                return e.href.match(/album/)}).text().trim();
+        }
+
         info.title    = $(".track_info .title").first().text();
         info.duration = Utils.calculateDuration($(".inline_player .track_info .time_total").text());
         info.elapsed  = Utils.calculateDuration($(".inline_player .track_info .time_elapsed").text());
