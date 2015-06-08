3f8ffe8d552d9bfff68b3bd206683c947ad01647
cgravolet <cgravolet@gmail.com>
2015-06-07 20:47:00 -0400

    Fixed minor issue with xbox scrobbling

--- src/js/plugins/xbox.js
+++ src/js/plugins/xbox.js
@@ -5,16 +5,27 @@ var Plugin = require("../modules/Plugin");
 var Utils  = require("../modules/Utilities");
 var xbox   = Object.create(Plugin).init("xbox", "Xbox Music", /music\.xbox\.com/i);
 
+xbox.test = function () {
+    var domainMatch = this.hostre.test(document.location.hostname);
+    var playerFound = $("#player").length > 0;
+
+    return domainMatch && playerFound;
+};
+
 xbox.scrape = function () {
     var $player         = $("#player");
     var $playerControls = $(".playerControls", $player);
     var $playerDuration = $(".playerDuration", $player);
     var $playerMetadata = $(".playerNowPlayingMetadata:visible", $player);
-    var artist          = $(".secondaryMetadata > a" ,$playerMetadata).attr("title");
-    var title           = $(".primaryMetadata > a" ,$playerMetadata).attr("title");
-    var duration        = $(".playerDurationTextRemaining", $playerDuration).text();
-    var elapsed         = $(".playerDurationTextOnGoing", $playerDuration).text();
+    var artist          = $.trim($(".secondaryMetadata > a" ,$playerMetadata).attr("title"));
+    var title           = $.trim($(".primaryMetadata > a" ,$playerMetadata).attr("title"));
+    var duration        = $.trim($(".playerDurationTextRemaining", $playerDuration).text());
+    var elapsed         = $.trim($(".playerDurationTextOnGoing", $playerDuration).text());
     var stopped         = $(".iconPlayerPlay", $playerControls).length ? true : false;
+
+    if (!artist.length || !title.length || !duration.length) {
+        return {};
+    }
 
     return {
         artist:   artist,
