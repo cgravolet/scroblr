80f8d7de7f2e86ecb6be13be1f4d5012adc59df6
Charles Gravolet <cgravolet@gmail.com>
2015-12-16 17:57:23 -0500

    Removed console message, cleaned up whitespace

--- src/js/plugins/yamusic.js
+++ src/js/plugins/yamusic.js
@@ -1,24 +1,23 @@
 "use strict";
 
-var $      = require("jquery");
-var Plugin = require("../modules/Plugin");
-var Utils  = require("../modules/Utilities");
+var $       = require("jquery");
+var Plugin  = require("../modules/Plugin");
+var Utils   = require("../modules/Utilities");
 var yamusic = Object.create(Plugin);
 
 yamusic.init("yamusic", "Yandex Music");
 
 yamusic.test = function() {
-    console.log(document.location.href);
     return (/music\.yandex\.ru/i).test(document.location.href);
 };
 
 yamusic.scrape = function() {
     return {
-        title: $(".player-controls .track__title").text(),
-        artist: $(".player-controls .track__artists a").first().text(),
+        title:    $(".player-controls .track__title").text(),
+        artist:   $(".player-controls .track__artists a").first().text(),
         duration: Utils.calculateDuration($(".progress__text progress__right").text() || ""),
         elapsed:  Utils.calculateDuration($(".progress__text progress__right").text() || ""),
-        stopped: $(".player-controls__btn_play").hasClass("player-controls__btn_pause"),
+        stopped:  $(".player-controls__btn_play").hasClass("player-controls__btn_pause"),
     };
 };
 
