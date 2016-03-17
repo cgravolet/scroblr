30b5b19e4f4e277c3d6e970b71e8547ba03dac4c
Charles Gravolet <cgravolet@gmail.com>
2016-03-16 20:07:42 -0400

    Fixed scrobbling on amazon music

--- src/js/plugins/amazon.js
+++ src/js/plugins/amazon.js
@@ -5,42 +5,21 @@ var Plugin = require("../modules/Plugin");
 var Utils  = require("../modules/Utilities");
 var amazon = Object.create(Plugin);
 
-amazon.init("amazon", "Amazon Cloud Player");
+amazon.init("amazon", "Amazon Music");
 
 amazon.test = function () {
     return (/amazon\.[A-Z\.]{2,}\/gp\/dmusic/i).test(document.location.href);
 };
 
 amazon.scrape = function () {
-    var $mp3Player   = $("#mp3Player");
-    var $nowPlaying  = $("#nowPlayingSection");
-    var $addlDetails = $nowPlaying.find(".currentSongAdditionalDetails");
-    var $details     = $nowPlaying.find(".currentSongDetails");
-    var $status      = $nowPlaying.find(".currentSongStatus");
-    var $mp3Controls = $mp3Player.find(".mp3Player-MasterControl");
-    
-    var songTitle = $details.find(".title").text();     // try to check for / fix truncated titles
-    if (songTitle.match(/\.\.\.$/)) {
-        if ($(".currentlyPlaying").length) {
-            songTitle = $(".currentlyPlaying td.titleCell").attr("title");
-        }
-        else if ($(".moreLikeThis").length) {
-            var moreLike = $(".moreLikeThis a").attr("href");
-            songTitle = moreLike.split("/title=")[1].split("/artist")[0];
-        }
-        else if ($(".recTriggerReason").length) {
-            songTitle = $(".recTriggerReason").text()
-                .replace("You are listening to ", "").replace(/\.$/, "");
-        }               
-    }
+    var $playbackInfo = $(".playbackControlsView");
+    var $scrubber = $(".scrubberTrack .scrubber", $playbackInfo);
 
     return {
-        album:    $addlDetails.find("span:last-child a").text(),
-        artist:   $details.find(".artistLink").attr("title"),
-        duration: Utils.calculateDuration($status.find("#currentDuration").text()),
-        elapsed:  Utils.calculateDuration($status.find("#currentTime").text()),
-        stopped:  $mp3Controls.find(".mp3MasterPlayGroup .mp3MasterPlay").hasClass("icon-play"),
-        title:    songTitle
+        artist:  $(".trackArtist > a", $playbackInfo).attr("title"),
+        percent: Math.round($scrubber.width() / $scrubber.parent().width() * 100) / 100,
+        stopped: $(".playbackControls span.playButton", $playbackInfo).hasClass("playerIconPlay"),
+        title:   $(".trackTitle", $playbackInfo).text()
     };
 };
 
