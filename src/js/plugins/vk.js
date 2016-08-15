c21cf7e4ed6639221f070da4ee3c2cf4b31292e8
4r2r <arthrrr@gmail.com>
2016-08-16 02:09:04 +0300

    Added support for new.vk.com (#108)

--- src/js/plugins/vk.js
+++ src/js/plugins/vk.js
@@ -7,18 +7,46 @@ var vk     = Object.create(Plugin);
 vk.init("vk", "VK");
 
 vk.scrape = function () {
-    var gpPlay = $("#gp_play");
-    var isPlaying = gpPlay && gpPlay.hasClass("playing");
-    var info = {
-        stopped: !isPlaying
-    };
+    function vknew() {
+        var info = { stopped: true };
 
-    if (isPlaying) {
-        info.artist = $("#gp_performer").text();
-        info.title =  $("#gp_title").text();
+        var player = $("#top_audio_player");
+        if(!player.length) { return info; }
+
+        var playing = player.hasClass("top_audio_player_playing");
+        if(!playing) { return info; }
+
+        var title =
+            player
+                .find('.top_audio_player_title')
+                .text()
+                .split('–'); // Unicode: EN DASH
+        if(title.length != 2) { return info; }
+
+        info.stopped = false;
+        info.artist  = title[0];
+        info.title   = title[1];
+
+        return info;
     }
 
-    return info;
+    function vkold() {
+        var gpPlay = $("#gp_play");
+        var isPlaying = gpPlay && gpPlay.hasClass("playing");
+        var info = {
+            stopped: !isPlaying
+        };
+
+        if (isPlaying) {
+            info.artist = $("#gp_performer").text();
+            info.title =  $("#gp_title").text();
+        }
+
+        return info;
+    }
+
+    var hostname = window.location.hostname;
+    return hostname.startsWith('new.vk') ? vknew() : vkold();
 };
 
 module.exports = vk;
