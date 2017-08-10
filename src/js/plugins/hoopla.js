3589155b5bbb1af6b7b196b24e8e2a7cd6728d6b
Brian Dukes <bdukes@engagesoftware.com>
2017-08-09 22:41:24 -0500

    Update Hoopla scrobbler
    Hoopla changed their interface and now uses a React-based app.
    
    This app is using "CSS in JS" and, therefore, no consistent class names to use for navigating the structure of the page, so the logic is fairly brittle.

--- src/js/plugins/hoopla.js
+++ src/js/plugins/hoopla.js
@@ -8,18 +8,37 @@ var hoopla = Object.create(Plugin);
 hoopla.init("hoopla", "Hoopla");
 
 hoopla.test = function () {
-    return (/\.hoopladigital\.com\/title\/\d+\?play/i).test(document.location.href);
+    return (/\.hoopladigital\.com\//i).test(document.location.href);
 };
 
 hoopla.scrape = function () {
-    var $playingRow = $('.playing');
-    var durationText = $.trim($playingRow.find('.segment-duration').text() || "");
-    var durationParts = /(?:(\d+)\smin)?\s*(?:(\d+)\ssec)?/.exec(durationText).slice(1);
+    var $progressSection = $('div[value]');
+    if ($progressSection.length === 0) {
+        return {};
+    }
+
+    var $progress = $progressSection.find('input[step]');
+    var durationInSeconds = $progress.attr('max');
+    var elapsedInSeconds = $progress.val();
+
+    var $trackInfo = $progressSection.prev();
+    var $title = $trackInfo.children().eq(1);
+
+    var $albumInfo = $progressSection.parent().parent().parent().prev();
+    var $albumText = $albumInfo.find('> div > div + div').first();
+
+    var TEXT_NODE_TYPE = 3;
+    var albumContents = $albumText.contents().filter(function (i, node) { return node.nodeType === TEXT_NODE_TYPE; });
+    if (albumContents.length < 3) {
+        return {};
+    }
+
     return {
-        album:    $(".title").text(),
-        artist:   $(".subheader").text(),
-        duration: durationParts.reverse().reduce(function (sum, t, i) { return sum + parseInt(t) * Math.pow(60, i); }, 0) * 1000,
-        title:    $playingRow.find('.segment-name').text()
+        album:    albumContents[0].data,
+        artist:   albumContents[2].data,
+        title:    $title.text(),
+        duration: durationInSeconds * 1000,
+        elapsed:  elapsedInSeconds * 1000,
     };
 };
 
